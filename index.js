55eae7944216d9b21f8f1696bc423daf78375046
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-03 20:43:04 -0700

    showo prompt and confirm and loops

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello bcpreppers');
+var count_to = prompt("How high should we count even numbers?")
+
+var sure = confirm('are you sure you want to count to ' + count_to + '?')
+if (sure) {
+    for (var i = 1; i < count_to; i++){
+        if (i % 2 === 0)
+            console.log("the number", i, "is even")
+    }
+}
+
